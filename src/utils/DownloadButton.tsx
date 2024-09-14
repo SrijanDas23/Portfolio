550593aa440481ac13ddf323b16cfe703c92e6a8
SrijanDas23 <157733127+SrijanDas23@users.noreply.github.com>
2024-09-14 13:48:31 +0530

    resume change

--- src/utils/DownloadButton.tsx
+++ src/utils/DownloadButton.tsx
@@ -1,11 +1,11 @@
 const DownloadButton = () => {
-  return (
-    <a href="./SrijanDas_resume.pdf" download="SrijanDas_resume.pdf">
-      <button className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:shadow-pink hover:text-white hover-up">
-        Download CV
-      </button>
-    </a>
-  );
+	return (
+		<a href="./SrijanDas_resume.pdf" download="Srijan Das resume.pdf">
+			<button className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:shadow-pink hover:text-white hover-up">
+				Download CV
+			</button>
+		</a>
+	);
 };
 
 export default DownloadButton;
